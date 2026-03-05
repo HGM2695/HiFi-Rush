f0684ad6dbb244177f3b1f4dd892ec858153a393
Cat Hanbit <gyumanhan4@gmail.com>
2026-03-05 11:51:48 +0900

    릴리즈 모드 에서도 제어 흐름 유지 - 기존엔 릴리즈 모드에서 GM_ASSERT 매크로들은 아무런 동작도 하지 않았지만, 릴리즈모드에서도 if문 검사, return, terminate와 같은 제어 흐름은 동일하게 가져가도록 변경

--- Engine/GMAssert.h
+++ Engine/GMAssert.h
@@ -4,17 +4,30 @@
 
 namespace gm
 {
-    void AssertPopupAndOptionalBreak(const char* tag, const char* exprText, const char* msg, const char* file, const char* func, int line);
+    void AssertPopupAndOptionalBreak(
+        const char* tag,
+        const char* exprText,
+        const char* msg,
+        const char* file,
+        const char* func,
+        int line);
 }
 
+// Debug, Release에서 ASSERT 매크로는 동일하게 사용하고, IMPL을 분리 함.
 #ifdef _DEBUG
+#define GM_ASSERT_IMPL(tag, exprText, msg, file, func, line) \
+    ::gm::AssertPopupAndOptionalBreak((tag), (exprText), (msg), (file), (func), (line))
+#else
+#define GM_ASSERT_IMPL(tag, exprText, msg, file, func, line) ((void)0)
+#endif // _DEBUG
+
 
 #define GM_ASSERT(expr, msg)                                                        \
 do                                                                                  \
 {                                                                                   \
     if (!(expr))                                                                    \
     {                                                                               \
-        ::gm::AssertPopupAndOptionalBreak("GM_ASSERT", #expr, (msg), __FILE__, __FUNCTION__, __LINE__); \
+        GM_ASSERT_IMPL("GM_ASSERT", #expr, (msg), __FILE__, __FUNCTION__, __LINE__);\
     }                                                                               \
 } while (0)
 
@@ -23,7 +36,7 @@ do                                                                              
 {                                                                                   \
     if (!(expr))                                                                    \
     {                                                                               \
-        ::gm::AssertPopupAndOptionalBreak("GM_ASSERT_RETURN", #expr, (msg), __FILE__, __FUNCTION__, __LINE__); \
+        GM_ASSERT_IMPL("GM_ASSERT_RETURN", #expr, (msg), __FILE__, __FUNCTION__, __LINE__); \
         return;                                                                     \
     }                                                                               \
 } while (0)
@@ -33,7 +46,7 @@ do                                                                              
 {                                                                                   \
     if (!(expr))                                                                    \
     {                                                                               \
-        ::gm::AssertPopupAndOptionalBreak("GM_ASSERT_RETURN_VAL", #expr, (msg), __FILE__, __FUNCTION__, __LINE__); \
+        GM_ASSERT_IMPL("GM_ASSERT_RETURN_VAL", #expr, (msg), __FILE__, __FUNCTION__, __LINE__); \
         return (val);                                                               \
     }                                                                               \
 } while (0)
@@ -43,16 +56,7 @@ do                                                                              
 {                                                                                   \
     if (!(expr))                                                                    \
     {                                                                               \
-        ::gm::AssertPopupAndOptionalBreak("GM_ASSERT_TERMINATE", #expr, (msg), __FILE__, __FUNCTION__, __LINE__); \
+        GM_ASSERT_IMPL("GM_ASSERT_TERMINATE", #expr, (msg), __FILE__, __FUNCTION__, __LINE__); \
         std::terminate();                                                           \
     }                                                                               \
 } while (0)
-
-#else
-
-#define GM_ASSERT(expr, msg)                 ((void)0)
-#define GM_ASSERT_RETURN(expr, msg)          ((void)0)
-#define GM_ASSERT_RETURN_VAL(expr, val, msg) ((void)0)
-#define GM_ASSERT_TERMINATE(expr, msg)       ((void)0)
-
-#endif // _DEBUG
