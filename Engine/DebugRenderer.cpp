d2992b221df95b2d99ba8d7b32668ff1a9ec75f0
Cat Hanbit <gyumanhan4@gmail.com>
2026-03-10 16:10:30 +0900

    디버그 드로우용 DebugRenderer 클래스 추가 - _DEBUG 빌드에서만 디버그 드로우 수행 - Rect, Line 등의 디버그 시각화 지원 - 충돌 영역, 카메라 영역 등의 디버깅에 활용 가능

---
+++ Engine/DebugRenderer.cpp
@@ -0,0 +1,44 @@
+﻿#include "DebugRenderer.h"
+#include "GMAssert.h"
+#include <windows.h>
+
+namespace gm::debug
+{
+	DebugRenderer::DebugRenderer(HDC hDC)
+		: _hDC(hDC)
+	{
+#ifdef _DEBUG
+		GM_ASSERT_RETURN(_hDC, "DC가 nullptr입니다.");
+#endif
+	}
+
+	DebugRenderer::~DebugRenderer() = default;
+
+	void DebugRenderer::DrawLine(int x1, int y1, int x2, int y2)
+	{
+#ifdef _DEBUG
+		MoveToEx(_hDC, x1, y1, nullptr);
+		LineTo(_hDC, x2, y2);
+#endif
+	}
+
+	void DebugRenderer::DrawRect(int left, int top, int right, int bottom)
+	{
+#ifdef _DEBUG
+		Rectangle(_hDC, left, top, right, bottom);
+#endif
+	}
+
+	void DebugRenderer::DrawCircle(int centerX, int centerY, int radius)
+	{
+#ifdef _DEBUG
+		Ellipse(
+			_hDC,
+			centerX - radius,
+			centerY - radius,
+			centerX + radius,
+			centerY + radius
+		);
+#endif
+	}
+}
---
+++ Engine/DebugRenderer.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+struct HDC__;
+typedef struct HDC__* HDC;
+
+namespace gm::debug
+{
+	class DebugRenderer
+	{
+	public:
+		explicit DebugRenderer(HDC hDC);
+		~DebugRenderer();
+
+	public:
+		void DrawLine(int x1, int y1, int x2, int y2);
+		void DrawRect(int left, int top, int right, int bottom);
+		void DrawCircle(int centerX, int centerY, int radius);
+
+	private:
+		HDC		_hDC = nullptr;
+	};
+}
