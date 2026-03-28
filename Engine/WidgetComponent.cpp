3954431977f01f88296e2ad3a70d84d79126f0f5
Cat Hanbit <gyumanhan4@gmail.com>
2026-03-28 16:05:44 +0900

    게임 오브젝트에 부착 가능한 WidgetComponent 추가
    - WidgetComponent를 추가해 게임 오브젝트에 부착되는 UI 표시 구조를 도입
    - 월드 위치를 스크린 위치로 변환한 뒤 UserWidget을 렌더하는 흐름을 엔진 쪽에 캡슐화

---
+++ Engine/WidgetComponent.cpp
@@ -0,0 +1,56 @@
+﻿#include "WidgetComponent.h"
+
+#include "Camera.h"
+#include "GameObject.h"
+#include "Transform.h"
+#include "UserWidget.h"
+
+namespace gm
+{
+	void WidgetComponent::SetWidgetVisible(bool isVisible)
+	{
+		if (_widget == nullptr)
+			return;
+
+		_widget->SetVisible(isVisible);
+	}
+
+	bool WidgetComponent::IsWidgetVisible() const
+	{
+		if (_widget == nullptr)
+			return false;
+
+		return _widget->IsVisible();
+	}
+
+	void WidgetComponent::ToggleWidgetVisibility()
+	{
+		if (_widget == nullptr)
+			return;
+
+		_widget->ToggleVisibility();
+	}
+
+	void WidgetComponent::OnInitialize()
+	{
+		_ownerTransform = GetOwner().GetComponent<Transform>();
+	}
+
+	void WidgetComponent::OnUpdate()
+	{
+		if (_widget == nullptr)
+			return;
+
+		_widget->Update();
+	}
+
+	void WidgetComponent::OnRender(HDC hDC)
+	{
+		if (_widget == nullptr || _ownerTransform == nullptr)
+			return;
+
+		const math::Vector2 worldPosition = _ownerTransform->GetPosition() + _worldOffset;
+		const math::Vector2 screenPosition = Camera::MainWorldToScreen(worldPosition);
+		_widget->Render(hDC, screenPosition);
+	}
+}
---
+++ Engine/WidgetComponent.h
@@ -0,0 +1,53 @@
+﻿#pragma once
+
+#include <memory>
+#include <type_traits>
+#include <utility>
+#include "Component.h"
+#include "Vector2.h"
+
+namespace gm
+{
+	class Transform;
+	class UserWidget;
+
+	class WidgetComponent : public Component
+	{
+	public:
+		WidgetComponent() = default;
+		~WidgetComponent() override = default;
+
+		template <typename T, typename... Args>
+		T* CreateUserWidget(Args&&... args)
+		{
+			static_assert(std::is_base_of_v<UserWidget, T>, "T는 반드시 UserWidget의 자식 클래스여야 합니다.");
+
+			auto widget = std::make_unique<T>(std::forward<Args>(args)...);
+			T* raw = widget.get();
+			raw->Initialize();
+
+			_widget = std::move(widget);
+			return raw;
+		}
+
+		void					SetWorldOffset(const math::Vector2& worldOffset) { _worldOffset = worldOffset; }
+		const math::Vector2&	GetWorldOffset() const { return _worldOffset; }
+
+		void					SetWidgetVisible(bool isVisible);
+		bool					IsWidgetVisible() const;
+		void					ToggleWidgetVisibility();
+
+		UserWidget*				GetWidget() { return _widget.get(); }
+		const UserWidget*		GetWidget() const { return _widget.get(); }
+
+	protected:
+		void OnInitialize() override;
+		void OnUpdate() override;
+		void OnRender(HDC hDC) override;
+
+	private:
+		Transform*					_ownerTransform = nullptr;
+		std::unique_ptr<UserWidget> _widget{};
+		math::Vector2				_worldOffset{};
+	};
+}
