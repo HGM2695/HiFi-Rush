ed4e002ae6509de426bd11b54c4d375847855273
Cat Hanbit <gyumanhan4@gmail.com>
2026-03-30 13:49:48 +0900

    디버그 입력 처리 시스템 추가
    - _DEBUG 환경에서만 디버그 입력이 동작하도록 처리
    - DebugType 비트 플래그 기반 활성화 제어
    - 문자열을 통한 매핑

---
+++ HiFi-Rush/DebugInputHandler.cpp
@@ -0,0 +1,54 @@
+﻿#include "DebugInputHandler.h"
+#include "../Engine/Application.h"
+#include "../Engine/Input.h"
+
+namespace gm
+{
+	bool DebugInputHandler::isTriggered(const std::wstring& eventName)
+	{
+#ifdef _DEBUG
+		const Input& input = APPLICATION.GetInput();
+
+		bool result = false;
+		if (_types & AudioTest)
+			result = DispatchAudioTest(eventName, input);
+		if (_types & PhysicsTest)
+			result |= DispatchPhysicsTest(eventName, input);
+
+		return result;
+#else
+		return false;
+#endif
+	}
+
+	bool DebugInputHandler::DispatchAudioTest(const std::wstring& eventName, const Input& input)
+	{
+#ifdef _DEBUG
+		if (eventName == L"Play" && input.IsKeyDown(KeyCode::P))
+			return true;
+		if (eventName == L"Stop" && input.IsKeyDown(KeyCode::S))
+			return true;
+		if (eventName == L"Pause" && input.IsKeyDown(KeyCode::D0))
+			return true;
+		if (eventName == L"Resume" && input.IsKeyDown(KeyCode::D1))
+			return true;
+		if (eventName == L"StopAll" && input.IsKeyDown(KeyCode::D2))
+			return true;
+
+
+		return false;
+#else
+		return false;
+#endif
+	}
+
+	bool DebugInputHandler::DispatchPhysicsTest(const std::wstring& eventName, const Input& input)
+	{
+#ifdef _DEBUG
+
+		return false;
+#else
+		return false;
+#endif
+	}
+}
---
+++ HiFi-Rush/DebugInputHandler.h
@@ -0,0 +1,67 @@
+﻿#pragma once
+
+#include <cstdint>
+#include <string>
+
+namespace gm
+{
+	class Input;
+
+	enum DebugType : uint32_t
+	{
+		None = 0u,
+		AudioTest = 1u << 0,
+		PhysicsTest = 1u << 1,
+
+		Count
+	};
+
+	class DebugInputHandler
+	{
+	public:
+		static inline void Enable(DebugType type)
+		{
+#ifdef _DEBUG
+			_types |= type;
+#else
+			(void)type;
+#endif
+		}
+
+		static inline void Disable(DebugType type)
+		{
+#ifdef _DEBUG
+			_types &= ~type;
+#else
+			(void)type;
+#endif
+		}
+
+		static inline void Toggle(DebugType type)
+		{
+#ifdef _DEBUG
+			_types ^= type;
+#else
+			(void)type;
+#endif
+		}
+
+		static inline void Clear()
+		{
+#ifdef _DEBUG
+			_types = 0;
+#endif
+		}
+
+		static bool isTriggered(const std::wstring& eventName);
+
+	private:
+		static bool DispatchAudioTest(const std::wstring& eventName, const Input& input);
+		static bool DispatchPhysicsTest(const std::wstring& eventName, const Input& input);
+
+	private:
+#ifdef _DEBUG
+		static inline uint32_t _types = AudioTest | PhysicsTest;
+#endif
+	};
+}
