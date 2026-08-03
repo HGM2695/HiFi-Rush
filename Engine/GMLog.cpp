a04a0ec9bcf65095f171305e0bb2fbf9daa77b0d
HGM2695 <gyumanhan4@gmail.com>
2026-08-03 15:13:28 +0900

    [Fix] GM_LOG 한글 출력 인코딩 수정
    - UTF-8 로그를 UTF-16으로 변환해 디버그 출력
    - 변환 실패 시 기존 ANSI 출력으로 폴백

--- Engine/GMLog.cpp
+++ Engine/GMLog.cpp
@@ -1,9 +1,9 @@
 #include "GMLog.h"
-
-#include <CoreWindow.h>
+#include "WindowsCore.h"
 
 #include <cstdarg>
 #include <cstdio>
+#include <iterator>
 
 namespace gm
 {
@@ -17,6 +17,12 @@ namespace gm
 
 		char outputBuffer[1024]{};
 		std::snprintf(outputBuffer, sizeof(outputBuffer), "[GM_LOG][%s] %s\n", level ? level : "", messageBuffer);
-		OutputDebugStringA(outputBuffer);
+
+		wchar_t wideOutputBuffer[1024]{};
+		const int convertedLength = MultiByteToWideChar(CP_UTF8, 0, outputBuffer, -1, wideOutputBuffer, static_cast<int>(std::size(wideOutputBuffer)));
+		if (convertedLength > 0)
+			OutputDebugStringW(wideOutputBuffer);
+		else
+			OutputDebugStringA(outputBuffer);
 	}
 }
